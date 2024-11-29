a93af48249354b352790f9269535c6c173b0d38e
safiya55 <safiyaedwards@trentu.ca>
2024-11-29 11:34:57 -0500

    Testing part 2
    testing if the borrow/reuturn methods function properly

--- Library/LibraryManagementTest.java
+++ Library/LibraryManagementTest.java
@@ -1,7 +1,11 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 public class LibraryManagementTest {
+	private Transaction transaction;
+	private Book book;
+	private Member member;
 
 	@Test
 	public void testBookId() throws Exception{
@@ -25,5 +29,39 @@ public class LibraryManagementTest {
 			assertEquals("Invalid book ID: 1000.", "Must be between 100 and 999");
 		}
 	}
+	
+	@Before
+	public void setUp() throws Exception {
+		transaction = Transaction.getTransaction();
+		book = new Book(101, "Book test");
+		member = new Member(1, "Member test");
+	}
+	
+	@Test
+	public void testBorrowReturn() throws Exception{
+		
+		//is book 
+		assertTrue("Book should initially be available", book.isAvailable());
+		
+		//borrow book
+		boolean borrowSuccess = transaction.borrowBook(book, member);
+		assertTrue("Borrowing should succeed for available books", borrowSuccess);
+		assertFalse("Borrowing should fail for unavailable books", book.isAvailable());
+		
+		//borrow the same book again
+		boolean borrowedAgain = transaction.borrowBook(book, member);
+		assertFalse("Borrowing should fail for unavailable books", borrowedAgain);
+		
+		//return book
+		boolean returnSuccess = transaction.returnBook(book, member);
+		assertTrue("Returning books should succeed", returnSuccess);
+		assertTrue("Book should now be available", book.isAvailable());
+		
+		//try returning the same book
+		boolean returnAgain = transaction.returnBook(book, member);
+		assertFalse("Returning should fail if book has already been returned", returnAgain);
+		
 
+	}
 }
+
